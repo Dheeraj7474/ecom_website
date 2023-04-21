6b7463ae12da5e16acbfefb11dfe5b07344091b9
Rajnish379 <rajinish77@gmail.com>
2023-04-21 13:46:56 +0530

    allowing only integers in form fields

---
+++ store/templates/script.js
@@ -0,0 +1,72 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const inputField = document.getElementById('revenue');
+
+    inputField.addEventListener('keypress', function(event) {
+        if (event.key === '.') {
+            event.preventDefault();
+        }
+    });
+});
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     const inputField = document.getElementById('costOfGoodsSold');
+
+//     inputField.addEventListener('keypress', function(event) {
+//         if (event.key === '.') {
+//             event.preventDefault();
+//         }
+//     });
+
+    
+// });
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     const inputField = document.getElementById('operatingExpenses');
+
+//     inputField.addEventListener('keypress', function(event) {
+//         if (event.key === '.') {
+//             event.preventDefault();
+//         }
+//     });
+
+    
+// });
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     const inputField = document.getElementById('depreciation');
+
+//     inputField.addEventListener('keypress', function(event) {
+//         if (event.key === '.') {
+//             event.preventDefault();
+//         }
+//     });
+
+    
+// });
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     const inputField = document.getElementById('interestExpense');
+
+//     inputField.addEventListener('keypress', function(event) {
+//         if (event.key === '.') {
+//             event.preventDefault();
+//         }
+//     });
+
+    
+// });
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     const inputField = document.getElementById('taxes');
+
+//     inputField.addEventListener('keypress', function(event) {
+//         if (event.key === '.') {
+//             event.preventDefault();
+//         }
+//     });
+
+    
+// });
+
+
+
